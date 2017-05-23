d9e050e7d3d95ed5273567bdf37c54cc7a5ded7e
muenchk <kai.muench@outlook.de>
2017-05-23 19:17:16 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -8,16 +8,26 @@ from tweet_text import idle_text
 # In unittest, test cases are represented by instances of unittest's TestCase class.
 # To make your own test cases you must write subclasses of TestCase.
 
-
 class TestTweetText(unittest.TestCase):
+    # This is called immediately before calling each test method
+    def setUp(self):
+        self.text1 = idle_text()
+        self.text2 = idle_text()
 
-    def test_idle(self):
+    def test_start(self):
         # In order to test something, we use one of the assert*()
         # methods provided by the TestCase base class
         # https://docs.python.org/2/library/unittest.html#unittest.TestCase.assertTrue
-        self.assertTrue( isinstance(idle_text(), str) )
+        Count = 0
+        for d in range (0, 100000):
+            self.text1 = idle_text()
+            self.text2 = idle_text()
+            if (not(self.text1 == self.text2)):
+                Count = Count + 1
+            
 
+        self.assertEqual(Count, 100000)
+        #self.assertNotEqual(self.text1, self.text2)
 
-
-#if __name__ == '__main__':
-#    unittest.main(verbosity=2)
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- tweet_text.py
+++ tweet_text.py
@@ -1,10 +1,9 @@
 """Construct messages to be sent as tweet text"""
-
+from random import random
 # Allows using time related functions
 from datetime import datetime
 # convert times according to time zones
 from pytz import timezone
-from requests import get
 
 def reply(tweet):
     """Return text to be used as a reply"""
@@ -20,7 +19,7 @@ def idle_text():
     """Return text that is tweeted when not replying"""
     # Construct the text we want to tweet out (140 chars max)
     berlin_time = datetime.now(timezone('Europe/Berlin'))
-    float i = random.random()
+    i = random()
     if i < 0.1:
         text = berlin_time.strftime("It is %H:%M:%S on a %A (%d-%m-%Y).")
     elif i < 0.2:
@@ -33,11 +32,6 @@ def idle_text():
         text = "Das elektrische Feld beschreibt einen Raumzustand um eine Punktladung Q"
     elif i < 0.6:
         text = "The mitochondria is the powerhouse of the cell."
-    elif i < 0.7:
-        text = "*insert creative text*"
     else:
-      # Some more ideas: https://www.programmableweb.com/category/humor/api
-      data = get('https://api.chucknorris.io/jokes/random').json()
-      joke = data['value']
-      text = joke[0:139]
+        text = "*insert creative text*"
     return text
